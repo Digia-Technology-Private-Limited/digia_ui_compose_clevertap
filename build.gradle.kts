6c5d608af40889ac1ccf4d36bd6a15f5980e28ec
Aditya Choubey <adityachoubey@digia.tech>
2026-02-21 13:23:59 +0530

    feat: Add showInLine

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,19 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21"
     id("maven-publish")
 }
 
 
-version = "1.0.0-beta.2"
+version = "1.0.0-beta.4"
 
 android {
     namespace = "com.digia.cleverTap"
     compileSdk = 36
 
     defaultConfig {
-        minSdk = 35
+        minSdk = 25
     }
 
     compileOptions {
@@ -23,11 +24,17 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+
+    buildFeatures {
+        compose = true
+    }
 }
 
 dependencies {
     implementation(libs.clevertap.android.sdk)
     implementation(libs.digia.ui.compose)
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
+    implementation("androidx.compose.runtime:runtime:1.7.6")
 }
 
 afterEvaluate {
